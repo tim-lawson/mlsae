97a3a21ef747eb136412f1d0b236e1473ba449fd
Tim Lawson <tim.lawson@bristol.ac.uk>
2024-09-09 17:01:40 +0100

    center vectors before cos sim

--- figures/resid_cos_sim.py
+++ figures/resid_cos_sim.py
@@ -13,20 +13,22 @@ from mlsae.utils import get_device, normalize
 
 
 class VarianceMetric:
-    def __init__(self) -> None:
+    def __init__(
+        self, size: tuple[int, ...] = (1,), device: torch.device | str = "cpu"
+    ) -> None:
         self.count = 0
-        self.mean = 0.0
-        self.squared = 0.0
+        self.mean = torch.zeros(size, device=device)
+        self.squared = torch.zeros(size, device=device)
 
     def update(self, x: torch.Tensor) -> None:
         self.count += x.shape[0]
         delta = x - self.mean
-        self.mean += torch.sum(delta).item() / self.count
-        self.squared += torch.sum(delta * delta).item()
+        self.mean += torch.sum(delta, dim=0) / self.count
+        self.squared += torch.sum(delta * delta, dim=0)
 
-    def compute(self) -> dict[str, float]:
+    def compute(self) -> dict[str, torch.Tensor]:
         var = self.squared / (self.count - 1)
-        std = math.sqrt(var)
+        std = var.sqrt()
         sem = std / math.sqrt(self.count)
         return dict(mean=self.mean, var=var, std=std, sem=sem)
 
@@ -44,20 +46,48 @@ def get_resid_cos_sim(
         device=device,
     )
     transformer.model.to(device)  # type: ignore
+
     data = DataModule(model_name=config.model_name, config=config.data)
     data.setup()
-    metrics = [VarianceMetric() for _ in range(transformer.n_layers - 1)]
 
+    resid_mean = [
+        VarianceMetric(size=(transformer.config.hidden_size,), device=device)
+        for _ in range(transformer.n_layers)
+    ]
+    resid_cos_sim = [
+        VarianceMetric(size=(1,), device=device)
+        for _ in range(transformer.n_layers - 1)
+    ]
+
+    # First, compute the mean residual stream activation vectors over the dataset
+    # https://www.lesswrong.com/s/6njwz6XdSYwNhtsCJ/p/eLNo7b56kQQerCzp2
     for i, batch in tqdm(enumerate(data._dataloader()), total=config.data.max_steps):
         x = transformer.forward(batch["input_ids"].to(device))
-        x = normalize(einops.rearrange(x, "l b p i -> l (b p) i"), -1)
+        x = einops.rearrange(x, "l b p i -> l (b p) i")
         for layer in range(transformer.n_layers - 1):
-            cos_sim = einops.einsum(x[layer], x[layer + 1], "bp i, bp i -> bp")
-            metrics[layer].update(cos_sim.flatten())
-
+            resid_mean[layer].update(x[layer, ...])
         if i > config.data.max_steps:
             break
-    return [metric.compute() for metric in metrics]
+
+    resid_mean = [metric.compute() for metric in resid_mean]
+    resid_mean = torch.stack([metric["mean"] for metric in resid_mean])  # l i
+    assert resid_mean.shape == (transformer.n_layers, transformer.config.hidden_size)
+
+    # Then, compute the mean cosine similarities between centered residual stream
+    # activation vectors at adjacent layers
+    for i, batch in tqdm(enumerate(data._dataloader()), total=config.data.max_steps):
+        x = transformer.forward(batch["input_ids"].to(device))
+        x = einops.rearrange(x, "l b p i -> l (b p) i")
+        x = x - resid_mean.unsqueeze(1)
+        x = normalize(x, -1)
+        for layer in range(transformer.n_layers - 1):
+            cos_sim = einops.einsum(x[layer], x[layer + 1], "bp i, bp i -> bp")
+            resid_cos_sim[layer].update(cos_sim.flatten())
+        if i > config.data.max_steps:
+            break
+
+    data = [metric.compute() for metric in resid_cos_sim]
+    return [{k: v.item() for k, v in layer.items()} for layer in data]
 
 
 def main(config: RunConfig, device: torch.device | str = "cpu") -> None:
