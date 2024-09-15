6254509f2de45574566754d347efb3e6f3567087
Tim Lawson <tim.lawson@bristol.ac.uk>
2024-09-15 14:17:16 +0100

    fix prompt opt

--- figures/prompt_heatmaps.py
+++ figures/prompt_heatmaps.py
@@ -51,7 +51,7 @@ def get_data(config: Config, repo_id: str, device: torch.device | str) -> torch.
 if __name__ == "__main__":
     device = get_device()
     config = parse(Config)
-    for repo_id in config.repo_ids(tuned_lens=True):
+    for repo_id in config.repo_ids(tuned_lens=config.tuned_lens):
         filename = f"prompt_heatmap_{repo_id.split('/')[-1]}.pdf"
         data = get_data(config, repo_id, device)
         save_heatmap(data.cpu(), os.path.join("out", filename), figsize=(5.5, 1.25))
