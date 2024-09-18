3ffad6c6d1b92ed1f816b237c1d2033a33090891
Tim Lawson <tim.lawson@bristol.ac.uk>
2024-09-18 07:48:47 +0100

    add lens to tests cfg

--- tests.py
+++ tests.py
@@ -1,9 +1,18 @@
+from dataclasses import dataclass
+
 from simple_parsing import parse
 from tqdm import tqdm
 
 from mlsae.model import DataConfig, MLSAEConfig
 from mlsae.trainer import RunConfig, SweepConfig, test
 from mlsae.utils import get_device
+
+
+@dataclass
+class Config(SweepConfig):
+    tuned_lens: bool = False
+    """Whether to apply a pretrained tuned lens before the encoder."""
+
 
 if __name__ == "__main__":
     device = get_device()
